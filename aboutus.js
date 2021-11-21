2e98b93c4aed6899cdd2f8f0b0107118e958dea3
HienBlueskytechco <hiendao07062000@gmail.com>
2021-11-22 00:49:45 +0700

    check

---
+++ aboutus.js
@@ -0,0 +1,9 @@
+$('.list__pictures').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+  });
